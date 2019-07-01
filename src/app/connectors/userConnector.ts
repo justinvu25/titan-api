da08cd1578133e7f7a2a27ef5adacf15ac51c15f
Justin Vu <justinvu25@gmail.com>
2019-07-01 14:29:19 -0400

    moved business logic from connector to model (#8)
    * moved business logic from connector to model
    
    * Made changes based on cr

--- src/app/connectors/userConnector.ts
+++ src/app/connectors/userConnector.ts
@@ -1,62 +1,25 @@
-import * as jwt from 'jsonwebtoken'
-import * as bcrypt from 'bcrypt'
-import { pick } from 'lodash'
 import User from '@/entities/user'
-import {
-	UserInput,
-	UserPayload,
-	LoginCredentials,
-	LoginPayload,
-} from '@/ts-types/user'
-import { SALT_ROUNDS, JWT_EXPIRY } from '@/utils/constants'
+import { UserInput, UserPayload } from '@/ts-types/user'
 
 class UserConnector {
+	async createUser(userData: UserInput): Promise<UserPayload> {
+		const { name, email, password } = userData
+		const user = await User.create({
+			name,
+			email,
+			password,
+		})
+		return user
+	}
+
 	async getAllUsers(): Promise<UserPayload[]> {
 		const users = await User.find()
 		return users
 	}
 
-	async registerUser(userData: UserInput): Promise<UserPayload> {
-		const { name, password, email } = userData
+	async findUserByEmail(email: string): Promise<UserPayload> {
 		const user = await User.findOne({ email })
-
-		if (!user) {
-			const hashedPassword = bcrypt.hashSync(password, SALT_ROUNDS)
-			if (hashedPassword) {
-				const createdUser = await User.create({
-					name,
-					email,
-					password: hashedPassword,
-				})
-				return createdUser
-			}
-			throw new Error('User could not be created.')
-		}
-		throw new Error('User already exists.')
-	}
-
-	async login(loginCredentials: LoginCredentials): Promise<LoginPayload> {
-		const { email, password } = loginCredentials
-		const user = await User.findOne({ email })
-
-		if (user) {
-			const hashedPassword = bcrypt.hashSync(password, SALT_ROUNDS)
-			if (bcrypt.compare(user.password, hashedPassword)) {
-				const token = jwt.sign(
-					{ user: pick(user, ['_id']) },
-					process.env.JWT_SECRET,
-					{
-						expiresIn: JWT_EXPIRY,
-					},
-				)
-				return {
-					accessToken: token,
-					expiresIn: JWT_EXPIRY,
-				}
-			}
-		}
-
-		throw new Error('Something went wrong.')
+		return user
 	}
 }
 
--- src/app/models/userModel.ts
+++ src/app/models/userModel.ts
@@ -1,14 +1,18 @@
+import * as bcrypt from 'bcrypt'
+import * as jwt from 'jsonwebtoken'
+import { pick } from 'lodash'
 import {
 	UserInput,
 	LoginCredentials,
 	LoginPayload,
 	UserPayload,
 } from '@/ts-types/user'
+import { SALT_ROUNDS, JWT_EXPIRY } from '@/utils/constants'
 
 interface UserConnector {
 	getAllUsers: Function
-	registerUser: Function
-	login: Function
+	createUser: Function
+	findUserByEmail: Function
 }
 
 class User {
@@ -23,11 +27,48 @@ class User {
 	}
 
 	async registerUser(userData: UserInput): Promise<UserPayload> {
-		return this.connector.registerUser(userData)
+		const user = await this.connector.findUserByEmail(userData.email)
+
+		if (user) {
+			throw new Error('User already exists.')
+		}
+
+		const hashedPassword = bcrypt.hashSync(userData.password, SALT_ROUNDS)
+		if (hashedPassword) {
+			const createdUser = await this.connector.createUser({
+				...userData,
+				password: hashedPassword,
+			})
+			return createdUser
+		}
+		throw new Error('User could not be created.')
 	}
 
 	async login(loginCredentials: LoginCredentials): Promise<LoginPayload> {
-		return this.connector.login(loginCredentials)
+		const user = await this.connector.findUserByEmail(loginCredentials.email)
+
+		if (!user) {
+			throw new Error('This user does not exist.')
+		}
+
+		const hashedPassword = bcrypt.hashSync(
+			loginCredentials.password,
+			SALT_ROUNDS,
+		)
+		if (bcrypt.compare(user.password, hashedPassword)) {
+			const token = jwt.sign(
+				{ user: pick(user, ['_id']) },
+				process.env.JWT_SECRET,
+				{
+					expiresIn: JWT_EXPIRY,
+				},
+			)
+			return {
+				accessToken: token,
+				expiresIn: JWT_EXPIRY,
+			}
+		}
+		throw new Error('Email or password is incorrect.')
 	}
 }
 
